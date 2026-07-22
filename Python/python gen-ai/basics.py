4995f775571b94b0671562989e89dd15ad00ceb8
bhateafnan8-crypto <bhateafnan8@gmail.com>
2026-07-22 16:18:41 +0530

    basics.py added to python gen ai folder -> python -> gen ai

---
+++ Python/python gen-ai/basics.py
@@ -0,0 +1,62 @@
+# Question-> 
+''' 
+1. Add comments to this code to explain what each line does:
+
+---
+name = "Alice"
+print("Hello", name)
+---
+'''
+
+# Soltuion-> 
+name = "Alice"  # name -> Variable name & Alice is value assign to name varibale as a string in double quotes
+print("Hello", name) # Here  print the value of name variable as a string + variable name -->>> Hello Allice
+
+
+
+# Question-> 
+'''
+2. Use escape sequences to format the following:
+
+---
+Output should be:
+Name:    John
+Age:     30
+He said, "I'm learning Python."
+---
+'''
+# Soltuion-> 
+print("---------------------------")
+print("\nOutput should be :")
+print("Name: \t John")
+print("Age: \t 30")
+
+
+
+# Question-> 
+'''
+3. Print this using only one print statement:
+
+---
+Apple | Banana | Mango <End>
+---
+'''
+
+# Soltuion-> 
+print("---------------------------")
+print("\n Apple" ," Banana " ," Mango ",sep=" | " ,end=" <END> \n ")
+
+
+# Question-> 
+'''
+4. (Challenge) Write a multi-line message using \n and \t to format it like:
+
+---
+Welcome to Python!
+	Learn to code
+		Have fun!
+---
+'''
+
+print("---------------------------")
+print("Welcome to Python! \n \t Learn to code \n \t \t  Have fun!")
