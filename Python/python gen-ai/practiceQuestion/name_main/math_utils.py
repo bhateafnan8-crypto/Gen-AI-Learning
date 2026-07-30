b279667c6603bbd281dd6332821d76ebe408154a
bhateafnan8-crypto <bhateafnan8@gmail.com>
2026-07-31 00:28:07 +0530

    solved name_main math_utils.py  question and succesfully added to  practiceQuestion-> python-genai->python->genai

---
+++ Python/python gen-ai/practiceQuestion/name_main/math_utils.py
@@ -0,0 +1,29 @@
+# Question-> 
+"""
+1. Create a file math_utils.py
+Define two functions:
+square(n)
+cube(n)
+Add test cases using if __name__ == "__main__" block.
+"""
+# Soltuion-> 
+print("---------------------------")
+
+def square(n):
+    return n * n
+
+def cube(n):
+    return n * n * n
+
+
+
+
+
+
+
+print("====== if__name__ == __main__ ======")
+
+
+if __name__ == "__main__" :
+    print(f"square function calling  => {square(3)}")
+    print(f"cube function calling  => {cube(3)}")
