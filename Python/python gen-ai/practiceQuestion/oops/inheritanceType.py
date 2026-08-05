5e8e853e185bdfda6fca9a260bbd7f7f91f1f1c5
bhateafnan8-crypto <bhateafnan8@gmail.com>
2026-08-05 23:34:37 +0530

    solved  inheritanceType.py  question and succesfully added to oops ->  practiceQuestion-> python-genai->python->genai

--- Python/python gen-ai/practiceQuestion/oops/inheritanceType.py
+++ Python/python gen-ai/practiceQuestion/oops/inheritanceType.py
@@ -5,6 +5,28 @@
 # Soltuion-> 
 print("---------------------------")
 
+#Base class
+class Person:
+    # instance attributes - constructor --
+    def __init__(self,name = "Adfar"):
+        self.name = name
+
+# child class - 1
+class Student(Person):
+    # instance attributes - constructor --
+    def getName(self):
+        return self.name
+
+# child class - 2
+class Teacher(Person):
+    # instance attributes - constructor --
+    def getName(self):
+        return self.name
+
+c1,c2 = Student(),Teacher()
+
+print(f"Student inherit name from person => {c1.getName()} || Teacher inherit name from person => {c2.getName()} ")
+
 
 
 
@@ -15,5 +37,27 @@ print("---------------------------")
 # Soltuion-> 
 print("---------------------------")
 
+#Base class 
+class Device:
+    #instance attribute - constructor
+    def __init__(self,name = "SamSung"):
+        self.name = name
 
+class Phone(Device):
+    #instance attribute - constructor
+    def __init__(self,modal = "Button-phone"):
+        super().__init__()
+        self.modal = modal
 
+class Smartphone(Phone):
+    #instance attribute - constructor
+    def __init__(self,invenstion = "ScreenTouch-phone"):
+        super().__init__()
+        self.invenstion = invenstion
+
+    def getDetail(self):
+        return f"Device name => {self.name} || Modal => {self.modal} || Invention to => {self.invenstion}"
+
+d1 = Smartphone()
+
+print(f"SmartPhone invention from device => ( {d1.getDetail()} )")
