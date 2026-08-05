a1f04626b226eba9526327abad4efd89f50aa78c
bhateafnan8-crypto <bhateafnan8@gmail.com>
2026-08-05 15:36:47 +0530

    solved  inheritance.py  question and succesfully added to oops ->  practiceQuestion-> python-genai->python->genai

--- Python/python gen-ai/practiceQuestion/oops/inheritance.py
+++ Python/python gen-ai/practiceQuestion/oops/inheritance.py
@@ -5,3 +5,24 @@
 """
 # Soltuion-> 
 print("---------------------------")
+
+class Employee:
+    # constructor
+    def __init__(self):
+        self.name = "adfar"
+        self.salary = 20000
+
+class Manager(Employee):
+    # constructor
+    def __init__(self):
+        super().__init__()
+        self.department = "IT department"
+
+    def get_details(self):
+        return f"department => {self.department} || name => {self.name} || salary => {self.salary}"
+
+safdar = Manager()
+
+print(f"safdar details => ({safdar.get_details()})")
+    
+    
---
+++ Python/python gen-ai/practiceQuestion/oops/staticmethod.py
@@ -0,0 +1,29 @@
+# Question-> 
+"""
+1. Create a class Geometry with static methods: area_of_circle(radius) and area_of_square(side).
+"""
+# Soltuion-> 
+print("---------------------------")
+
+
+
+
+# Question-> 
+"""
+2. Create a class Validator with a static method is_valid_email(email) that checks if @ is present.
+"""
+# Soltuion-> 
+print("---------------------------")
+
+
+
+
+# Question-> 
+"""
+3. Modify your previous OOP mini project and add one static method for a utility operation (e.g., formatting, cleaning input, etc.).
+"""
+# Soltuion-> 
+print("---------------------------")
+
+
+
