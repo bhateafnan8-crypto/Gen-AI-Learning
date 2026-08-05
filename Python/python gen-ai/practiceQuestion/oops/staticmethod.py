0a863bdbcf004c2d295c2ec62af9965a83ee2eba
bhateafnan8-crypto <bhateafnan8@gmail.com>
2026-08-05 23:11:32 +0530

    solved staticmethod.py  question and succesfully added to oops ->  practiceQuestion-> python-genai->python->genai

--- Python/python gen-ai/practiceQuestion/oops/staticmethod.py
+++ Python/python gen-ai/practiceQuestion/oops/staticmethod.py
@@ -4,6 +4,24 @@
 """
 # Soltuion-> 
 print("---------------------------")
+class Geometry:
+    #constructor -> instance method
+    def __init__(self):
+        self.aoc = Geometry.area_of_circle(4)
+        self.aos = Geometry.area_of_square(4)
+
+    @staticmethod
+    def area_of_circle(radius):
+        return (radius**2)*3.14
+    
+    def area_of_square(side):
+        return side ** 2
+    
+# print(Geometry.area_of_circle(4))
+# print(Geometry.area_of_square(4))
+geometry1 = Geometry()
+print(f"area of circle => {geometry1.aoc} || area of square => {geometry1.aos}")
+
 
 
 
@@ -15,6 +33,23 @@ print("---------------------------")
 # Soltuion-> 
 print("---------------------------")
 
+class Validator:
+    # instance attribute - constructor
+    def __init__(self):
+        self.email = Validator.is_valid_email("Adfar@gmail.com")
+
+    @staticmethod
+    def  is_valid_email(email):
+        if "@" in email:
+            return email
+        else:
+            return "Invalid email"
+
+print(Validator.is_valid_email("Adfargmail.com"))
+email1 = Validator()
+
+print(f"Email 1 validation => {email1.email}")
+
 
 
 
@@ -25,5 +60,29 @@ print("---------------------------")
 # Soltuion-> 
 print("---------------------------")
 
+class Validators:
+    # instance attribute - constructor
+    def __init__(self):
+        self.email = Validators.is_valid_email(Validators.format_text("Adfar@gmail.com"))
+    
+    @staticmethod
+
+    def format_text(email):
+        text = email.strip()
+        lowertext = text.lower()
+        return lowertext
+
+    @staticmethod
+    
+    def  is_valid_email(email):
+        if "@" in email:
+            return email
+        else:
+            return "Invalid email"
+
+print(Validators.is_valid_email(Validators.format_text("Adfargmail.com")))
+email1 = Validators()
+
+print(f"Email 1 validation => {email1.email}")
 
 
