cdc812dac7516bae829d71f0cf4470546f00754e
bhateafnan8-crypto <bhateafnan8@gmail.com>
2026-08-16 02:04:53 +0530

    solved exception.py   question and succesfully added to   practiceQuestion-> python-genai->python->genai

---
+++ Python/python gen-ai/practiceQuestion/exception.py
@@ -0,0 +1,115 @@
+# Question-> 
+"""
+1. Write a program that opens a file and reads 
+   its content. Use try-except-finally to handle 
+   FileNotFoundError.
+"""
+# Soltuion-> 
+print("---------------------------")
+import json
+try:
+    with open("settings.json","r") as f :
+        load = json.load(f)
+
+except FileNotFoundError:
+    print("File Not Found!")
+
+
+# Question-> 
+"""
+2. Ask the user for a positive number. 
+   Raise a ValueError if the input is zero or negative.
+"""
+# Soltuion-> 
+print("---------------------------")
+print("\n sol 1 custom errors of negative \n")
+class NegativeError(Exception):
+    pass
+
+try:
+    user = int(input("Enter positive number => "))
+    if user < 0:
+        raise NegativeError("Number should not be negative")
+    print(user)
+
+except NegativeError as e:
+    print("Error :",e)
+
+print("\n sol 2 VaueError of zero and negative \n")
+
+try:
+    user = int(input("Enter positive number => "))
+    if user <= 0:
+        raise ValueError("Number should not be Zero or  negative")
+    print(user)
+
+except ValueError as e:
+    print("Error :",e)
+
+
+# Question-> 
+"""
+3. Create a custom exception OutOfRangeError and 
+   raise it if a number is not between 1 and 100.
+"""
+# Soltuion-> 
+print("---------------------------")
+
+class OutOfRangeError(Exception):
+    pass
+
+try:
+    num = int(input("Enter number between (1-100) => "))
+    if 100 < num or num < 1:
+        raise OutOfRangeError("number is out of range (1-100) ",num)
+    print(num)
+except OutOfRangeError as e:
+    print("Error",e)
+
+
+# Question-> 
+"""
+4. Write a program that divides two numbers. 
+   Catch and handle ZeroDivisionError and ValueError. 
+   Use finally to print "Done."
+"""
+# Soltuion-> 
+print("---------------------------")
+try:
+    n1 = int(input("Enter number 1 => "))
+    n2 = int(input("Enter number 2 => "))
+
+    result = n1 / n2
+
+    print(result)
+except ValueError:
+    print("Numbers should be integer")
+except ZeroDivisionError:
+    print("number 2 should not be zero")
+finally:
+    print("Done")
+
+
+# Question-> 
+"""
+5. Modify the password validation example to raise 
+   a custom exception if:
+   - Password is < 6 characters
+   - It doesn't contain a digit
+   (Hint: Create a WeakPasswordError class.)
+"""
+# Soltuion-> 
+print("---------------------------")
+
+class WeakPasswordError(Exception):
+    pass
+
+try:
+    passwords = input("Enter password (min -6) only char => ")
+
+    if len(passwords) < 6 or any(char.isdigit() for char in passwords):
+        raise WeakPasswordError(f"Password {passwords} is weak becuase contains digit and len is less than 6")
+    print(passwords)
+
+except WeakPasswordError as e:
+    print("Error",e)
--- Python/python gen-ai/practiceQuestion/modules-packages/modules-packages.py
+++ Python/python gen-ai/practiceQuestion/modules-packages/modules-packages.py
@@ -1,62 +1,62 @@
 import os 
 import sys
-# # Question-> 
-# """
-# 1. os module use karo:
-#    - Current directory print karo
-#    - Ek folder banao "test_folder"
-#    - Check karo exist karta hai ya nahi
-#    - Delete karo
-# """
-# # Soltuion-> 
-# print("---------------------------")
-# print(os.getcwd())
+# Question-> 
+"""
+1. os module use karo:
+   - Current directory print karo
+   - Ek folder banao "test_folder"
+   - Check karo exist karta hai ya nahi
+   - Delete karo
+"""
+# Soltuion-> 
+print("---------------------------")
+print(os.getcwd())
 
-# os.mkdir("test_folder")
-# print(os.path.exists("test_folder"))
+os.mkdir("test_folder")
+print(os.path.exists("test_folder"))
 
-# os.rmdir("test_folder")
+os.rmdir("test_folder")
 
 
-# # Question-> 
-# """
-# 2. sys module use karo:
-#    - Python version print karo
-#    - Current script ka path print karo
-#    - sys.argv use karke command line 
-#      se naam lo aur print karo
-# """
-# # Soltuion-> 
-# print("---------------------------")
+# Question-> 
+"""
+2. sys module use karo:
+   - Python version print karo
+   - Current script ka path print karo
+   - sys.argv use karke command line 
+     se naam lo aur print karo
+"""
+# Soltuion-> 
+print("---------------------------")
 
-# print(sys.version)
+print(sys.version)
 
-# print(sys.argv[0])
+print(sys.argv[0])
 
-# print(sys.argv[1])
+print(sys.argv[1])
 
-# if len(sys.argv) > 1:
-#     print(f"Hello! {sys.argv[1]}")
-# else:
-#     print("There is no arguments provided")
+if len(sys.argv) > 1:
+    print(f"Hello! {sys.argv[1]}")
+else:
+    print("There is no arguments provided")
 
-# # Question-> 
-# """
-# 3. Apna ek module banao "calculator.py" mein:
-#    add, subtract, multiply, divide functions
-#    Phir alag file mein import karke use karo
-# """
-# # Soltuion-> 
-# print("---------------------------")
+# Question-> 
+"""
+3. Apna ek module banao "calculator.py" mein:
+   add, subtract, multiply, divide functions
+   Phir alag file mein import karke use karo
+"""
+# Soltuion-> 
+print("---------------------------")
 
-# import calc
+import calc
 
-# calc.add(2,3)
-# calc.sub(4,2)
-# calc.mul(2,3)
-# calc.div(5,2)
-# calc.div(5,0)
-# # calc.div(5,"b")
+calc.add(2,3)
+calc.sub(4,2)
+calc.mul(2,3)
+calc.div(5,2)
+calc.div(5,0)
+# calc.div(5,"b")
 
 
 # Question-> 
