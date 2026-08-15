f56e5c3cd45a0508fc0a53990b27f9ee95197868
bhateafnan8-crypto <bhateafnan8@gmail.com>
2026-08-15 13:08:32 +0530

    solved modules-packages.py   question and succesfully added to modules-packages ->  practiceQuestion-> python-genai->python->genai

---
+++ Python/python gen-ai/practiceQuestion/modules-packages/modules-packages.py
@@ -0,0 +1,68 @@
+import os 
+import sys
+# Question-> 
+"""
+1. os module use karo:
+   - Current directory print karo
+   - Ek folder banao "test_folder"
+   - Check karo exist karta hai ya nahi
+   - Delete karo
+"""
+# Soltuion-> 
+print("---------------------------")
+print(os.getcwd())
+
+os.mkdir("test_folder")
+print(os.path.exists("test_folder"))
+
+os.rmdir("test_folder")
+
+
+# Question-> 
+"""
+2. sys module use karo:
+   - Python version print karo
+   - Current script ka path print karo
+   - sys.argv use karke command line 
+     se naam lo aur print karo
+"""
+# Soltuion-> 
+print("---------------------------")
+
+print(sys.version)
+
+print(sys.argv[0])
+
+print(sys.argv[1])
+
+
+
+# Question-> 
+"""
+3. Apna ek module banao "calculator.py" mein:
+   add, subtract, multiply, divide functions
+   Phir alag file mein import karke use karo
+"""
+# Soltuion-> 
+print("---------------------------")
+
+import calc
+
+calc.add(2,3)
+calc.sub(4,2)
+calc.mul(2,3)
+calc.div(5,2)
+calc.div(5,0)
+# calc.div(5,"b")
+
+
+
+
+
+
+
+
+
+
+# modules-packages
+# modules-packages.py
