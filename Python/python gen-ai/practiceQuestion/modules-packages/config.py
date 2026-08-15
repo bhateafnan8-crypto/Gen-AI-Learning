185890d53c74048b3bb5098532bbe697a5ca3c75
bhateafnan8-crypto <bhateafnan8@gmail.com>
2026-08-16 01:15:51 +0530

    solved modules-packages.py   question and succesfully added to modules-packages ->  practiceQuestion-> python-genai->python->genai

---
+++ Python/python gen-ai/practiceQuestion/modules-packages/config.py
@@ -0,0 +1,16 @@
+# Question-> 
+"""
+8. Ek "config.py" module banao jisme 
+   sirf constants hon:
+   DB_NAME = "myapp.db"
+   MAX_RETRIES = 3
+   DEBUG = True
+   Phir main file mein import karke use karo
+   aur ek function banao jo config print kare
+"""
+# Soltuion-> 
+print("---------------------------")
+
+DB_NAME = "myapp.db"
+MAX_RETRIES = 3
+DEBUG = True
