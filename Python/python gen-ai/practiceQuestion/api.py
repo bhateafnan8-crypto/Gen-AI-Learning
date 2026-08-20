d5f1d28fc5a1d0ec3584dfc484917a9608f2fbb0
bhateafnan8-crypto <bhateafnan8@gmail.com>
2026-08-20 18:38:13 +0530

    solved api.py   question and succesfully added to  practiceQuestion-> python-genai->python->genai

--- Python/python gen-ai/practiceQuestion/api.py
+++ Python/python gen-ai/practiceQuestion/api.py
@@ -1,3 +1,4 @@
+import requests
 # Question-> 
 """
 1. https://jsonplaceholder.typicode.com/posts/1
@@ -7,6 +8,25 @@
 # Soltuion-> 
 print("---------------------------")
 
+try:
+    response = requests.get("https://jsonplaceholder.typicode.com/posts/1",timeout=5)
+    response.raise_for_status()
+    print(f"get status : {response.status_code}")
+    print("-" * 20)
+
+    print(f"json data : {response.json()}")
+    print("-" * 20)
+
+    json_data = response.json()
+    print(f"title : {json_data['title']}")
+    print("-" * 20)
+
+    print(f"body : {json_data['body']}")
+    
+   #  print(f"title : {response['title']}")
+   #  print(f"body : {response['body']}")
+except requests.exceptions.RequestException as e:
+    print(f"Error : {e}")
 
 
 # Question-> 
@@ -19,6 +39,21 @@ print("---------------------------")
 # Soltuion-> 
 print("---------------------------")
 
+try:
+   data_post = {"title": "test", "body": "hello", "userId": 1}
+   response1 = requests.post("https://jsonplaceholder.typicode.com/posts",json=data_post,timeout=10)
+   response1.raise_for_status()
+   print(f"status code : {response1.status_code}")
+   print("-" * 20)
+
+   print(f"json data : {response1.json()}")
+   print("-" * 20)
+
+   post_data = response1.json()
+   print(f"id : {post_data['id']}")
+
+except requests.exceptions.RequestException as e:
+    print(f"Error : {e}")
 
 
 # Question-> 
@@ -30,7 +65,19 @@ print("---------------------------")
 # Soltuion-> 
 print("---------------------------")
 
+try:
+    response2 = requests.get("https://jsonplaceholder.typicode.com/posts/999",timeout=5)
+    response2.raise_for_status()
+    json_fetch = response2.json()
+    print(f"status code : {response2.status_code}")
+    print(f"json data : {json_fetch}")
+    print("-" * 20)
 
+    print(f"title : {json_data['title']}")
+    print("-" * 20)
+
+except requests.exceptions.RequestException as e:
+   print(f"Error : {e}")
 
 
 # api.py
