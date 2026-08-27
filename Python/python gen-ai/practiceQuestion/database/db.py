c916047627e9bdbdece5a9c3ae65f85c0446335d
bhateafnan8-crypto <bhateafnan8@gmail.com>
2026-08-27 14:09:56 +0530

    solved db.py  question and succesfully added to  practiceQuestion-> python-genai->python->genai

--- Python/python gen-ai/practiceQuestion/database/db.py
+++ Python/python gen-ai/practiceQuestion/database/db.py
@@ -1,1 +1,36 @@
+# Question-> 
+"""
+1. "students.db" banao — students table create karo
+   (id, name, marks) — 3 records insert karo,
+   phir sab fetch karke print karo
+"""
+# Soltuion-> 
+print("---------------------------")
+
+
+# Question-> 
+"""
+2. Wahi DB se sirf un students ko 
+   fetch karo jinka marks > 40 hai
+"""
+# Soltuion-> 
+print("---------------------------")
+
+
+# Question-> 
+"""
+3. Ek student ka marks update karo naam se,
+   phir delete karo naam se —
+   dono ke baad SELECT se verify karo
+"""
+# Soltuion-> 
+print("---------------------------")
+
+
+
+
+
+
+
+
 # db.py
