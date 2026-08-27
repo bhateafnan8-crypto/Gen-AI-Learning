ba78a3c11886fc3357d2955399c8a27ef8803456
bhateafnan8-crypto <bhateafnan8@gmail.com>
2026-08-27 16:59:36 +0530

    solved db.py  question and succesfully added to  practiceQuestion-> python-genai->python->genai

--- Python/python gen-ai/practiceQuestion/database/db.py
+++ Python/python gen-ai/practiceQuestion/database/db.py
@@ -1,3 +1,4 @@
+import sqlite3
 # Question-> 
 """
 1. "students.db" banao — students table create karo
@@ -7,6 +8,37 @@
 # Soltuion-> 
 print("---------------------------")
 
+conn = sqlite3.connect("students.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+   CREATE TABLE IF NOT EXISTS students(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      marks INTEGER
+   )
+""")
+
+cursor.execute("INSERT INTO students (name,marks) VALUES (?,?)",("Adfar",30))
+conn.commit()
+
+cursor.execute("INSERT INTO students (name,marks) VALUES (?,?)",("Safdar",40))
+conn.commit()
+
+cursor.execute("INSERT INTO students (name,marks) VALUES (?,?)",("asfar",50))
+conn.commit()
+
+
+
+cursor.execute("SELECT * FROM students")
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+conn.close()
+
 
 # Question-> 
 """
@@ -15,6 +47,35 @@ print("---------------------------")
 """
 # Soltuion-> 
 print("---------------------------")
+
+conn = sqlite3.connect("students1.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+   CREATE TABLE IF NOT EXISTS students1(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      marks INTEGER
+   )
+""")
+
+cursor.execute("INSERT INTO students1 (name,marks) VALUES (?,?)",("Adfar",30))
+conn.commit()
+
+cursor.execute("INSERT INTO students1 (name,marks) VALUES (?,?)",("Safdar",40))
+conn.commit()
+
+cursor.execute("INSERT INTO students1 (name,marks) VALUES (?,?)",("asfar",50))
+conn.commit()
+
+cursor.execute("SELECT * FROM students1 WHERE marks > ?",(40,))
+
+toppr = cursor.fetchall()
+
+for top in toppr:
+    print(top)
+
+conn.close()
 
 
 # Question-> 
@@ -26,7 +87,48 @@ print("---------------------------")
 # Soltuion-> 
 print("---------------------------")
 
+conn = sqlite3.connect("students2.db")
+cursor = conn.cursor()
 
+cursor.execute("""
+   CREATE TABLE IF NOT EXISTS students2(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      marks INTEGER
+   )
+""")
+
+cursor.execute("INSERT INTO students2 (name,marks) VALUES (?,?)",("Adfar",30))
+conn.commit()
+
+cursor.execute("INSERT INTO students2 (name,marks) VALUES (?,?)",("Safdar",40))
+conn.commit()
+
+cursor.execute("INSERT INTO students2 (name,marks) VALUES (?,?)",("asfar",50))
+conn.commit()
+
+
+cursor.execute("UPDATE students2 SET marks = ? WHERE name = ?",(20,"Adfar"))
+conn.commit()
+
+cursor.execute("SELECT * FROM students2")
+
+toppr1 = cursor.fetchall()
+
+for top in toppr1:
+    print(top)
+
+cursor.execute("DELETE FROM students2 WHERE name = ?",("Safdar",))
+conn.commit()
+
+cursor.execute("SELECT * FROM students2")
+
+toppr1 = cursor.fetchall()
+
+for top in toppr1:
+    print(top)
+
+conn.close()
 
 
 
